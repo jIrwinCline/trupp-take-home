ffd12f0a3b8588f41c6bf54acb26401f09a48419
jirwincline <jci@pdx.edu>
2020-06-16 06:39:47 -0700

    add interface for mock data

--- src/data/mock.ts
+++ src/data/mock.ts
@@ -1,9 +1,44 @@
+interface EmployeeDetails {
+  contactName: String;
+  dateOfBirth: Date;
+  maskedSocialSecurity: String;
+  employmentStatus: String;
+  hireDate: Date;
+  streetAddress: {
+    addressLine1: String;
+    addressLine2: String | null;
+    city: String;
+    state: String;
+    zip: String;
+  };
+  mailingAddress: {
+    addressLine1: String | null;
+    addressLine2: String | null;
+    city: String | null;
+    state: String | null;
+    zip: String | null;
+  };
+  personalEmail: String;
+  // personalEmail: null,
+  workEmail: String;
+  // workEmail: null,
+  mobilePhone: String;
+  // mobilePhone: null,
+  workPhone: String;
+  // workPhone: null,
+  genderDescription: String;
+  medicareEligible: boolean | null;
+  smoker: boolean | null;
+  disabled: boolean | null;
+  student: boolean | null;
+}
+
 export const EMPLOYEEDETAILS_MOCK: EmployeeDetails = {
-  contactName: 'Carrie Conway',
-  dateOfBirth: new Date('February 7, 1990'),
-  maskedSocialSecurity: '*1234',
-  employmentStatus: 'Part-Time',
-  hireDate: new Date('January 15, 2019'),
+  contactName: "Carrie Conway",
+  dateOfBirth: new Date("February 7, 1990"),
+  maskedSocialSecurity: "*1234",
+  employmentStatus: "Part-Time",
+  hireDate: new Date("January 15, 2019"),
   // streetAddress: {
   //   addressLine1: '12345 Aldergrove Street',
   //   addressLine2: null,
@@ -19,11 +54,11 @@ export const EMPLOYEEDETAILS_MOCK: EmployeeDetails = {
   //   zip: '97221'
   // },
   streetAddress: {
-    addressLine1: '12345 Aldergrove Street',
+    addressLine1: "12345 Aldergrove Street",
     addressLine2: null,
-    city: 'Portland',
-    state: 'OR',
-    zip: '97217',
+    city: "Portland",
+    state: "OR",
+    zip: "97217",
   },
   mailingAddress: {
     addressLine1: null,
@@ -32,15 +67,15 @@ export const EMPLOYEEDETAILS_MOCK: EmployeeDetails = {
     state: null,
     zip: null,
   },
-  personalEmail: 'carrieconway@gmail.com',
+  personalEmail: "carrieconway@gmail.com",
   // personalEmail: null,
-  workEmail: 'carrie.conway@work.com',
+  workEmail: "carrie.conway@work.com",
   // workEmail: null,
-  mobilePhone: '555-555-5555',
+  mobilePhone: "555-555-5555",
   // mobilePhone: null,
-  workPhone: '222-222-2222',
+  workPhone: "222-222-2222",
   // workPhone: null,
-  genderDescription: 'Female',
+  genderDescription: "Female",
   medicareEligible: null,
   smoker: null,
   disabled: null,
