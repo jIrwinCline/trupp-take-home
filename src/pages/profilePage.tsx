1c8a57282c336e928add52a8ec0210ebd5dbd542
jirwincline <jci@pdx.edu>
2020-06-16 06:52:34 -0700

    add center styling to profile page title

--- src/pages/profilePage.tsx
+++ src/pages/profilePage.tsx
@@ -1,11 +1,12 @@
 import React, { ReactElement } from "react";
+import "../styles/profile-page-styles.css";
 
 interface Props {}
 
 export default function profilePage({}: Props): ReactElement {
   return (
     <div>
-      <h1>My User Profile</h1>
+      <h1 className="header">My User Profile</h1>
     </div>
   );
 }
