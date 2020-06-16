1a3bdacb7df15d29f69e2bb8032e0b69d7b1e793
jirwincline <jci@pdx.edu>
2020-06-16 06:49:57 -0700

    reorganize App into components dir

---
+++ src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/App.css";
+
+//components
+import ProfilePage from "../pages/profilePage";
+
+function App() {
+  return (
+    <div>
+      <ProfilePage />
+    </div>
+  );
+}
+
+export default App;
