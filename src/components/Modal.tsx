46b114248fcad60e3be1a05ccca88a9d09004283
jirwincline <jci@pdx.edu>
2020-06-16 07:18:57 -0700

    add MUI dialog c omponents  for modal pieces

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -5,13 +5,57 @@ import { EMPLOYEEDETAILS_MOCK as data } from "../data/mock";
 
 //MUI
 import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 interface Props {}
 
 export default function Modal({}: Props): ReactElement {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="center-btn">
-      <Button>Edit Details</Button>
+      <Button variant="outlined" onClick={handleOpen}>
+        Edit Details
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
+        <DialogTitle id="form-dialog-title">title</DialogTitle>
+        <DialogContent>
+          <DialogContentText>blah blah blah</DialogContentText>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
